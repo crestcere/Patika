8693933fb7584b7758b7bbe7057c0794e714bc9c
iCarly <crestcere@outlook.com>
2023-12-07 07:55:52 +0300

    Validate Forms, Submit Button

--- Front-End/Fimple/Ödev 1/js.js
+++ Front-End/Fimple/Ödev 1/js.js
@@ -1,11 +1,9 @@
-console.log("JS Loaded");
 let items;
 let loaded = false;
 async function getData() {
     try {
         const response = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=5");
         if (response.ok) {
-            // const items = await response.json();
             items = await response.json();
             printTable(items);
         } else {
@@ -19,6 +17,10 @@ async function getData() {
 getData();
 
 const printTable = (items) => {
+
+    // items.push({title: "Does it works?", completed: true});
+
+
     // Create temporary string that it will attach to innerHTML
     const right = document.querySelector(".right");
     let rights = `
@@ -32,25 +34,52 @@ const printTable = (items) => {
         rights += `
             <tr>
                 <td>${item.title}</td>
-                <td>${item.completed}</td>
-            </tr>
         `;
+        if (item.completed) {
+            rights += `
+                    <td><input type="checkbox" checked disabled </td>
+                </tr>
+            `;
+        } else {
+                rights += `
+                    <td><input type="checkbox" disabled </td>
+                </tr>
+            `;
+        }
     }
-    rights += `
-            <tr>
-                <td>Does it works?</td>
-                <td>Yes</td>
-            </tr>
-        </table>
-    `;
 
     // Bind string to innerHTML
     right.innerHTML = rights;
-
 }
 
+const inputTitle = document.querySelector("#ftitle");
+const inputValue = document.querySelector("#fcompleted");
 
+// Validate Forms
+const formValidate = () => {
+    if (inputTitle.value === "") {
+        alert("Title cannot be empty!");
+        return false;
+    } else {
+        return true;
+    }
+}
 
-// items.forEach((item) => {
-//     console.log(item);
-// });
+// Clear form
+const formClear = () => {
+    inputTitle.value = "";
+    inputValue.checked = false;
+}
+
+// Submit Button
+const form = document.querySelector("#formSubmit");
+form.onsubmit = async (e) => {
+    e.preventDefault();
+    if (formValidate()) {
+        items.push({title: inputTitle.value, completed: inputValue.checked});
+        printTable(items);
+        formClear();
+    }
+}
+
+// TODO: Loading
