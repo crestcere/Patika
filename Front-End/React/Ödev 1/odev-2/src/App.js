90d3f65369689ed214d2455d1b7c0a4fae640503
iCarly <crestcere@outlook.com>
2023-12-16 09:55:03 +0300

    Initial Commit

---
+++ Front-End/React/Ödev 1/odev-2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Getdata from "./components/Getdata.js"
+
+function App() {
+  return (
+    <div className="App">
+        <input name={id} />
+        <Getdata id={2}></Getdata>
+    </div>
+  );
+}
+
+export default App;
