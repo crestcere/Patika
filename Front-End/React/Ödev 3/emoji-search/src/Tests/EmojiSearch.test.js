0255a281144b77e5287fee6edd2939cd7dafca8e
iCarly <crestcere@outlook.com>
2024-01-01 14:02:48 +0300

    Initial Commit

---
+++ Front-End/React/Ödev 3/emoji-search/src/Tests/EmojiSearch.test.js
@@ -0,0 +1,46 @@
+// Başlık kısmının başarılı bir şekilde render edildiğini kontrol edecek olan test kodunu yazın.
+// Uygulama ilk açıldığında emoji listesinin başarılı bir şekilde render edildiğini kontrol edecek olan test kodunu yazın.
+// Bir filtreleme işlemi yapıldığında, emoji listesinin bu filtreye uygun şekilde yeniden render edildiğini kontrol edecek olan test kodunu yazın.
+// Liste üzerinden herhangi emojiye tıklandığında, ilgili emojinin kopyalandığını kontrol edecek olan test kodunu yazın.
+import React from "react"
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import '@testing-library/jest-dom';
+
+import emojiList from "../emojiList.json";
+import App from "../App";
+
+describe("Emoji Search Test", ()=>{
+    let header,emoji,input,filterList;
+    beforeEach(() =>{
+        render(<App/>);
+    });
+    test('header render test', () => {
+        header = screen.getByText(/Emoji Search/i);
+        expect(header).toBeInTheDocument();//header text kontrolü
+        const images = screen.getAllByRole("img");
+        expect(images[0]);
+        expect(images[1]);// 2 tane img olması lazım
+    })
+    test("emoji list control",()=>{
+        emoji = emojiList.slice(0,19); // ilk 20 elamnı alalım
+        //listedeki ilk 20 eleman ekranda gösterilmişmi kontrol
+        emoji.map((item) =>{expect(screen.getByText(item.title)).toBeInTheDocument();});
+    })
+
+    test("emoji list filter and re-render", () => {
+        input = screen.getByRole("textbox"); //inputa ulaşma
+        const filter = "smile cat";
+        filterList = emojiList.filter(it => it.keywords.toLowerCase().match(filter) || it.title.toLowerCase().match(filter));
+        fireEvent.change(input,{target:{value:filter}});
+        expect(screen.getAllByText(/cat/i)).toHaveLength(2);
+    });
+
+    it("proof that after click emoji,copy", async () => {
+        const click = screen.getByText("Joy");
+        expect(click.parentElement.getAttribute("data-clipboard-text").length).toBeGreaterThan(0);
+        console.log(click.parentElement.getAttribute("data-clipboard-text"));
+        expect(click.parentElement.getAttribute("data-clipboard-text")).toMatch("😂");
+    });
+
+})
